3226519bc0a58e7e4078344f504f2fd04fef6d21
angeluciel <jpmizidoro@gmail.com>
2026-08-27 13:31:06 -0300

    register done

--- apps/web/src/app/[lang]/(auth)/register/_components/steps.tsx
+++ apps/web/src/app/[lang]/(auth)/register/_components/steps.tsx
@@ -20,7 +20,6 @@ import { Link } from "@/i18n/navigation";
 import { signInWithGoogle } from "@/lib/api/register";
 import { useFieldError } from "@/lib/form";
 import type { StepProps } from "@/types/register";
-import { useFieldError } from "/lib/form";
 
 const LINK_CLASS =
 	"text-text-link hover:text-text-link-pressed visited:text-text-lnik-visited hover:visited:text-text-link-visited-pressed";
