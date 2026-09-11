ba037ca0714febe8504bbfeaf565909740efad9d
angeluciel <jpmizidoro@gmail.com>
2026-09-11 20:51:32 +0000

    proxy.ts can't use string.raw

--- apps/web/src/proxy.ts
+++ apps/web/src/proxy.ts
@@ -174,6 +174,6 @@ export async function proxy(request: NextRequest) {
 
 export const config: ProxyConfig = {
 	matcher: [
-		String.raw`/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt|.*\.(?:svg|png|jpg|jpeg|gif|webp|avif|ico)$).*)`,
+		"/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt|.*\\.(?:svg|png|jpg|jpeg|gif|webp|avif|ico)$).*)",
 	],
 };
